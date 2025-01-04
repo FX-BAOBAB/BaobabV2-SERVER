6a9280fda116b9d4c43f5a5e1c42c51a7e2c6f89
seongeun308 <kse011010@gmail.com>
2025-01-04 15:29:07 +0900

    BV2-7 (fix): ImageProducer send() 매개변수 수정
    ImageResponse responses -> String topic, ImageResponse response

--- file/src/main/java/file/application/port/output/ImageProducer.java
+++ file/src/main/java/file/application/port/output/ImageProducer.java
@@ -5,5 +5,5 @@ import file.domain.ImageResponse;
 import java.util.List;
 
 public interface ImageProducer {
-    void send(ImageResponse responses);
+    void send(String topic, ImageResponse response);
 }
