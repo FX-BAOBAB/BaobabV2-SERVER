deffbe0f15fc1065e66533d1d510a10a165de1a1
seongeun308 <kse011010@gmail.com>
2025-01-10 00:41:05 +0900

    BV2-7 (refactor): PR 리뷰사항 적용
    - 이미지를 저장할 디렉토리 생성 되지 않을 시 ImageStorageException 처리
    - 이미지 저장 성공 및 실패 로깅 적용

--- file/src/main/java/file/adapter/output/LocalFileStorage.java
+++ file/src/main/java/file/adapter/output/LocalFileStorage.java
@@ -4,7 +4,6 @@ import file.application.port.output.FileDirStorage;
 import file.core.common.error.ImageErrorCode;
 import file.core.common.exception.image.ImageStorageException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,16 +17,14 @@ import java.nio.file.StandardCopyOption;
 @Slf4j
 public class LocalFileStorage implements FileDirStorage {
 
-
     @Override
     public void store(Path filePath, MultipartFile imageFile) {
 
         File directory = new File(filePath.getParent().toString());
         if (!directory.exists()) {
             boolean mkDirs = directory.mkdirs();
-            // TODO Exception 처리 필요
             if (!mkDirs) {
-                throw new RuntimeException("DIR 생성 실패!");
+                throw new ImageStorageException(ImageErrorCode.IMAGE_STORAGE_PATH_ERROR);
             }
         }
 
@@ -35,10 +32,13 @@ public class LocalFileStorage implements FileDirStorage {
             Files.copy(imageFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             throw new ImageStorageException(ImageErrorCode.IMAGE_STORAGE_ERROR);
-        }finally {
-            // TODO Image Upload Logging 필요, 실패 시 실패 사유 분석 용
+        } finally {
+            if (Files.exists(filePath)) {
+                log.info("File exists at: {}", filePath);
+            } else {
+                log.error("Image upload failed, file not found at: {}", filePath);
+            }
         }
-
         log.info("Uploaded image to {}", filePath);
     }
 }
