21097a0c09674133f2c25feff133cfb1cd22533d
seongeun308 <kse011010@gmail.com>
2025-01-14 01:37:26 +0900

    BV2-61 (feat): 단일 이미지 삭제 구현
    BV2-61 (feat): 단일 이미지 삭제 구현
    
    - MongoDBImageMetaDataMetaDataRepository -> MongoDBImageMetaDataRepository 클래스명 변경

--- file/src/main/java/file/application/port/output/MongoDBImageMetaDataRepository.java
+++ file/src/main/java/file/application/port/output/MongoDBImageMetaDataRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MongoDBImageMetaDataMetaDataRepository extends MongoRepository<ImageMetaData, ObjectId>, ImageMetaDataPersistencePort {
+public interface MongoDBImageMetaDataRepository extends MongoRepository<ImageMetaData, ObjectId>, ImageMetaDataPersistencePort {
     ImageMetaData save(ImageMetaData metaData);
+    ImageMetaData deleteById(String imageId);
+    boolean existsById(String imageId);
 }
