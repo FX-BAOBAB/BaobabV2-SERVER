64a98fbb3022953accca874cd6bde91025db7885
seongeun308 <kse011010@gmail.com>
2025-01-15 02:27:36 +0900

    BV2-68 (fix): ObjectId -> String 변경

--- file/src/main/java/file/application/port/output/MongoDBImageMetaDataRepository.java
+++ file/src/main/java/file/application/port/output/MongoDBImageMetaDataRepository.java
@@ -1,12 +1,11 @@
 package file.application.port.output;
 
 import file.domain.ImageMetaData;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MongoDBImageMetaDataRepository extends MongoRepository<ImageMetaData, ObjectId>, ImageMetaDataPersistencePort {
+public interface MongoDBImageMetaDataRepository extends MongoRepository<ImageMetaData, String>, ImageMetaDataPersistencePort {
     ImageMetaData save(ImageMetaData metaData);
     ImageMetaData deleteById(String imageId);
     boolean existsById(String imageId);
