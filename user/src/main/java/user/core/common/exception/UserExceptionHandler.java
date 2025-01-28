4afe9099a73aee76bf249ae93a28a874faf79c97
SEOB <svj7531@naver.com>
2025-01-28 23:58:24 +0900

    BV2-103 (Refactor) : Exception Handler 로그 수정
    BV2-103 (Refactor) : Exception Handler 로그 수정

--- user/src/main/java/user/core/common/exception/UserExceptionHandler.java
+++ user/src/main/java/user/core/common/exception/UserExceptionHandler.java
@@ -19,35 +19,35 @@ public class UserExceptionHandler {
 
     @ExceptionHandler(value = UserExistsException.class)
     public ResponseEntity<Api<Object>> existsUserException(UserExistsException e) {
-        log.warn("", e);
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.CONFLICT)
             .body(Api.ERROR(UserErrorCode.USER_EXISTS));
     }
 
     @ExceptionHandler(value = EmailExistsException.class)
     public ResponseEntity<Api<Object>> existsEmailException(EmailExistsException e) {
-        log.warn("", e);
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.CONFLICT)
             .body(Api.ERROR(UserErrorCode.EMAIL_EXISTS));
     }
 
     @ExceptionHandler(value = NickNameExistsException.class)
     public ResponseEntity<Api<Object>> existsNickNameException(NickNameExistsException e) {
-        log.warn("", e);
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.CONFLICT)
             .body(Api.ERROR(UserErrorCode.NICKNAME_EXISTS));
     }
 
     @ExceptionHandler(value = UserNotFoundException.class)
     public ResponseEntity<Api<Object>> notFoundUserException(UserNotFoundException e) {
-        log.warn("", e);
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
             .body(Api.ERROR(UserErrorCode.USER_NOT_FOUND));
     }
 
     @ExceptionHandler(value = PasswordMismatchException.class)
     public ResponseEntity<Api<Object>> mismatchPasswordException(PasswordMismatchException e) {
-        log.warn("", e);
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body(Api.ERROR(UserErrorCode.PASSWORD_MISMATCH));
     }
