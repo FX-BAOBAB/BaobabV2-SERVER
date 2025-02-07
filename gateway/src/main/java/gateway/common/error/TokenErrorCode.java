2c89616ef134290cf12be339e7c31c314ae3362b
JIUNG YANG <yangjiung286@gmail.com>
2025-02-07 16:34:58 +0900

    BV2-22 (Refactor) : Gateway 인증 처리 및 Exception, Handler 추가

--- gateway/src/main/java/gateway/common/error/TokenErrorCode.java
+++ gateway/src/main/java/gateway/common/error/TokenErrorCode.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum TokenErrorCode implements ErrorCodeIfs {
 
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), 1102,"유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED.value(),1103,"만료된 토큰입니다."),
+    TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED.value(),1104,"알 수 없는 토큰 에러입니다."),
     NOT_PERMITTED(HttpStatus.FORBIDDEN.value(),1105,"허용되지 않은 접근입니다.")
     ;
 
