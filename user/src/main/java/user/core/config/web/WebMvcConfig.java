611c7fea16f65904b0fa7509d82455b6403d1806
SEOB <svj7531@naver.com>
2025-02-12 17:19:09 +0900

    BV2-151 (!HOTFIX) : CORS 허용

--- user/src/main/java/user/core/config/web/WebMvcConfig.java
+++ user/src/main/java/user/core/config/web/WebMvcConfig.java
@@ -32,7 +32,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "https://baobab.run")
+            .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
