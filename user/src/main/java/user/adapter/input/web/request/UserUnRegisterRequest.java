fd7c351e256ef53423e1e2ff479df277524ef8de
SEOB <svj7531@naver.com>
2025-02-14 20:33:24 +0900

    BV2-157 (Refactor) : 비밀번호 정규식 변경
    BV2-157 (Refactor) : 비밀번호 정규식 변경
    
    - 특수문자 규칙 제거

--- user/src/main/java/user/adapter/input/web/request/UserUnRegisterRequest.java
+++ user/src/main/java/user/adapter/input/web/request/UserUnRegisterRequest.java
@@ -12,8 +12,8 @@ import lombok.NoArgsConstructor;
 public class UserUnRegisterRequest {
 
     @Pattern(
-        regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA-Z0-9])(?!.*[\\\\{}()<>$%^&*_=|`]).{8,100}$",
-        message = "대문자, 소문자, 특수문자를 포함하고 8자 이상이어야 합니다."
+        regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{8,100}$",
+        message = "대문자, 소문자, 숫자를 포함하고 8자 이상이어야 합니다."
     )
     @NotBlank(message = "필수 입력 사항입니다.")
     private String password;
