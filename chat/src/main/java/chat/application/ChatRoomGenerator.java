af9c8586bcff48a97aa7a2ea280b39c66ff58253
SEOB <svj7531@naver.com>
2025-02-24 22:09:56 +0900

    BV2-159 (Refactor) : 외부 호출 시 자동으로 전략을 주입하도록 변경
    BV2-159 (Refactor) : 외부 호출 시 자동으로 전략을 주입하도록 변경

--- chat/src/main/java/chat/application/ChatRoomGenerator.java
+++ chat/src/main/java/chat/application/ChatRoomGenerator.java
@@ -1,6 +1,8 @@
 package chat.application;
 
 import chat.application.stragety.ChatRoomTitleStrategy;
+import chat.application.stragety.NormalTitleGenerator;
+import chat.application.stragety.TruncatedTitleGenerator;
 import java.util.List;
 import lombok.Setter;
 
@@ -9,7 +11,14 @@ public class ChatRoomGenerator {
 
     private ChatRoomTitleStrategy chatRoomChatRoomTitleStrategy;
 
-    public String generateTitle(List<String> nickNameList) {
+    private static final int MAX_USER_TITLE = 5;
+
+    public String generateDefaultTitle(List<String> nickNameList) {
+        if (nickNameList.size() > MAX_USER_TITLE) {
+            this.setChatRoomChatRoomTitleStrategy(new TruncatedTitleGenerator());
+        } else {
+            this.setChatRoomChatRoomTitleStrategy(new NormalTitleGenerator());
+        }
         return chatRoomChatRoomTitleStrategy.generateTitle(nickNameList);
     }
 
--- chat/src/test/java/chat/application/ChatRoomGeneratorTest.java
+++ chat/src/test/java/chat/application/ChatRoomGeneratorTest.java
@@ -2,8 +2,6 @@ package chat.application;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import chat.application.stragety.NormalTitleGenerator;
-import chat.application.stragety.TruncatedTitleGenerator;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -16,10 +14,9 @@ class ChatRoomGeneratorTest {
     void 채팅방_제목_생성_성공() {
         // Given
         ChatRoomGenerator chatRoomGenerator = new ChatRoomGenerator();
-        chatRoomGenerator.setChatRoomChatRoomTitleStrategy(new NormalTitleGenerator());
 
         // When
-        String generatedTitle = chatRoomGenerator.generateTitle(List.of("닉네임1", "닉네임2", "닉네임3"));
+        String generatedTitle = chatRoomGenerator.generateDefaultTitle(List.of("닉네임1", "닉네임2", "닉네임3"));
 
         // Then
         assertEquals("닉네임1, 닉네임2, 닉네임3의 채팅방", generatedTitle);
@@ -30,7 +27,6 @@ class ChatRoomGeneratorTest {
     void 채팅방_제목_생성_100명_성공() {
         // Given
         ChatRoomGenerator chatRoomGenerator = new ChatRoomGenerator();
-        chatRoomGenerator.setChatRoomChatRoomTitleStrategy(new TruncatedTitleGenerator());
 
         List<String> nickNameList = new ArrayList<>();
         for (int i = 1; i <= 100; i++) {
@@ -38,7 +34,7 @@ class ChatRoomGeneratorTest {
         }
 
         // When
-        String generatedTitle = chatRoomGenerator.generateTitle(nickNameList);
+        String generatedTitle = chatRoomGenerator.generateDefaultTitle(nickNameList);
 
         // Then
         assertEquals("닉네임1, 닉네임2, 닉네임3, 닉네임4, 닉네임5 외 95명의 채팅방", generatedTitle);
