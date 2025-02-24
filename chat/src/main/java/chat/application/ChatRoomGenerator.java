cced96dc3afca6f637645dc376dec2c8cbb86de4
SEOB <svj7531@naver.com>
2025-02-24 20:53:50 +0900

    BV2-159 (Refactor) : 책임 분할
    BV2-159 (Refactor) : 책임 분할
    
    1. StringBuilder 제거, join 으로 대체

--- chat/src/main/java/chat/application/ChatRoomGenerator.java
+++ chat/src/main/java/chat/application/ChatRoomGenerator.java
@@ -9,24 +9,22 @@ public class ChatRoomGenerator implements ChatRoomTitleGeneratorUseCase {
     @Override
     public String generateDefaultTitle(List<String> userNickNameList) {
         if (userNickNameList.size() > MAX_USER_TITLE) {
-            return generateTitle(userNickNameList.subList(0, MAX_USER_TITLE))
-                + " 외 " + (userNickNameList.size() - MAX_USER_TITLE) + "명의 채팅방";
+            return truncateTitle(userNickNameList);
         }
+        return generateNormalTitle(userNickNameList);
+    }
+
+    private String truncateTitle(List<String> userNickNameList) {
+        return generateTitle(userNickNameList.subList(0, MAX_USER_TITLE))
+            + " 외 " + (userNickNameList.size() - MAX_USER_TITLE) + "명의 채팅방";
+    }
+
+    private String generateNormalTitle(List<String> userNickNameList) {
         return generateTitle(userNickNameList) + "의 채팅방";
     }
 
     private String generateTitle(List<String> userNickNameList) {
-        StringBuilder titleBuilder = new StringBuilder();
-        boolean first = true;
-
-        for (String userNickName : userNickNameList) {
-            if (!first) {
-                titleBuilder.append(", ");
-            }
-            titleBuilder.append(userNickName);
-            first = false;
-        }
-
-        return titleBuilder.toString();
+        return String.join(", ", userNickNameList);
     }
+
 }
