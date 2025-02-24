7f02a7528e886dcbfd3572816e4c243abf15bc5c
SEOB <svj7531@naver.com>
2025-02-24 16:43:21 +0900

    BV2-158 (Refactor) : UserChat 복합키 설정
    BV2-158 (Refactor) : UserChat 복합키 설정

--- chat/src/main/java/chat/adapter/output/persistence/repository/document/UserChatDocument.java
+++ chat/src/main/java/chat/adapter/output/persistence/repository/document/UserChatDocument.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -12,7 +14,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "user_chat")
+@CompoundIndex(def = "{'userId': 1, 'chatRoomId': 1}", unique = true) // 복합키 설정
 public class UserChatDocument {
+
+    @Id
+    private String id;
 
     private String userId;
 
