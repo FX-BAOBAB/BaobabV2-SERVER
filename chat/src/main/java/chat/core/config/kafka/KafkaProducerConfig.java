07ab915854c30e859f858d2564cbd23a524646f3
SEOB <svj7531@naver.com>
2025-02-25 18:53:00 +0900

    BV2-162 (Fix) : Kafka config 수정
    BV2-162 (Fix) : Kafka config 수정
    
    1. JsonSerializer 로 수정

--- chat/src/main/java/chat/core/config/kafka/KafkaProducerConfig.java
+++ chat/src/main/java/chat/core/config/kafka/KafkaProducerConfig.java
@@ -5,12 +5,14 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.RoundRobinPartitioner;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @EnableKafka
 @Configuration
@@ -23,8 +25,8 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, ChatMessage> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, ChatMessage.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ChatMessage.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         configProps.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,
             RoundRobinPartitioner.class.getName());
@@ -36,5 +38,5 @@ public class KafkaProducerConfig {
     public KafkaTemplate<String, ChatMessage> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
-    
+
 }
