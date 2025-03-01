1388a8cf0b3e301d50117a7abbdd63e604a4987a
SEOB <svj7531@naver.com>
2025-03-02 01:10:32 +0900

    BV2-169 (Refactor) : Redis 에 userId:server address 저장하도록 변경
    BV2-169 (Refactor) : Redis 에 userId:server address 저장하도록 변경
    
    - 채팅방 접속 시 Redis 에 `userId : server address` 저장

--- chat/src/main/java/chat/application/ChatRoomConnectionService.java
+++ chat/src/main/java/chat/application/ChatRoomConnectionService.java
@@ -4,6 +4,8 @@ import chat.application.port.input.ChatRoomConnectionUseCase;
 import chat.application.port.output.SseConnectionRegistryPort;
 import chat.application.sse.SseEmitterManager;
 import chat.application.sse.UserSseConnection;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -16,17 +18,20 @@ public class ChatRoomConnectionService implements ChatRoomConnectionUseCase {
     private final SseEmitterManager sseEmitterManager;
     private final StringRedisTemplate redisTemplate;
 
-    private final static String CONNECTED_USER = "connectedUsers";
-
     @Override
     public void connectChatRoom(String userId) {
-        redisTemplate.opsForSet().add(CONNECTED_USER, userId);
+        try {
+            String serverAddress = InetAddress.getLocalHost().getHostAddress();
+            redisTemplate.opsForValue().set(userId, serverAddress);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(); // TODO EXCEPTION
+        }
         sseConnectionRegistryPort.saveEmitter(userId, sseEmitterManager.createSseEmitter(userId));
     }
 
     @Override
     public void disconnectChatRoom(String userId, UserSseConnection connection) {
-        redisTemplate.opsForSet().remove(CONNECTED_USER, userId);
+        redisTemplate.delete(userId);
         sseConnectionRegistryPort.deleteEmitter(connection);
     }
 
