869f2bcec378016b58e8c38fd78af1e89dffc608
SEOB <svj7531@naver.com>
2025-03-02 21:21:35 +0900

    BV2-171 (!HOTFIX) : Global module 내 kafka 코드 주석처리
    BV2-171 (!HOTFIX) : Global module 내 kafka 코드 주석처리

--- global/src/main/java/global/message/TestMessageProducerImpl.java
+++ global/src/main/java/global/message/TestMessageProducerImpl.java
@@ -1,20 +1,20 @@
-package global.message;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
-
-@Slf4j
+//package global.message;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
 //@Component
-@RequiredArgsConstructor
-public class TestMessageProducerImpl implements MessageProducer<UserMessage> {
-
-    private final KafkaTemplate<String, UserMessage> kafkaTemplate;
-
-    @Override
-    public void send(String topic, UserMessage message) {
-        kafkaTemplate.send(topic, message);
-    }
-
-}
+//@RequiredArgsConstructor
+//public class TestMessageProducerImpl implements MessageProducer<UserMessage> {
+//
+//    private final KafkaTemplate<String, UserMessage> kafkaTemplate;
+//
+//    @Override
+//    public void send(String topic, UserMessage message) {
+//        kafkaTemplate.send(topic, message);
+//    }
+//
+//}
