253ad3c717b735e78c99a0f91b4ede606895a43b
SEOB <svj7531@naver.com>
2025-03-07 00:41:04 +0900

    BV2-163 (Fix): 누락된 @Request Body 추가
    BV2-163 (Fix): 누락된 @Request Body 추가

--- chat/src/main/java/chat/adapter/input/web/ChatApiController.java
+++ chat/src/main/java/chat/adapter/input/web/ChatApiController.java
@@ -50,7 +50,7 @@ public class ChatApiController {
     }
 
     @PostMapping("/feign/message")
-    public void sendMessage(ChatMessage chatMessage) {
+    public void sendMessage(@RequestBody ChatMessage chatMessage) {
         log.info("Feign 메시지 수신 : {}", chatMessage);
         messageDispatchUseCase.dispatchMessage(chatMessage);
     }
