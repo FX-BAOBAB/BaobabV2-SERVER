31dd290ada93d82e9532969965493e8819123cf8
SEOB <svj7531@naver.com>
2025-03-21 21:57:10 +0900

    !HOTFIX: FeignClient url path 추가

--- chat/src/main/java/chat/adapter/output/client/ArticleClient.java
+++ chat/src/main/java/chat/adapter/output/client/ArticleClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "baobab-article", url = "https://baobab-article")
+@FeignClient(name = "baobab-article", url = "https://baobab.run")
 public interface ArticleClient {
 
     @GetMapping("/userId")
