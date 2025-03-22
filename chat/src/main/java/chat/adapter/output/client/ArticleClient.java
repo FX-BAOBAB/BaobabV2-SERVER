b8468d25634fb888dfb7a84f51fed0a4ff88ce47
SEOB <svj7531@naver.com>
2025-03-22 14:39:51 +0900

    BV2-195 (!HOTFIX) : FeignClient url 환경변수 처리
    BV2-195 (!HOTFIX) : FeignClient url 환경변수 처리
    
    1. CloudFlare 403 error 로 인해
    'http://ip/article-service/open-api' 처리

--- chat/src/main/java/chat/adapter/output/client/ArticleClient.java
+++ chat/src/main/java/chat/adapter/output/client/ArticleClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "baobab-article", url = "https://baobab.run/article-service/open-api")
+@FeignClient(name = "baobab-article", url = "${feign.article-service.url}")
 public interface ArticleClient {
 
     @GetMapping("/simple-info")
