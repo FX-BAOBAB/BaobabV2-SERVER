500af07d0ca59ef9a87d91f857a1b4a641c25e7f
JIUNG YANG <yangjiung286@gmail.com>
2025-04-06 03:06:15 +0900

    BV2-216 (Feat) : 지수 백오프와 지터로 메시지 재전송 기능 구현

--- message/src/main/java/message/adapter/output/message/FcmMessageSender.java
+++ message/src/main/java/message/adapter/output/message/FcmMessageSender.java
@@ -24,6 +24,12 @@ public class FcmMessageSender implements SendMessagePort {
 
     private final Executor messageCallbackExecutor;
 
+    private static final int MAX_ATTEMPTS = 5;
+
+    private static final int INITIAL_DELAY_MS = 10000;
+
+    private static final int DELAY_MULTIPLIER = 2;
+
     @Override
     public void send(Message message) {
         ApiFuture<String> apiFuture = firebaseMessaging.sendAsync(message);
@@ -31,6 +37,9 @@ public class FcmMessageSender implements SendMessagePort {
             try {
                 apiFuture.get();
             } catch (ExecutionException e) {
+                if (shouldRetry(e)) {
+                    retrySendWithBackoff(message);
+                }
                 throw new FailedToSendMessageException(MessageErrorCode.FAILED_TO_SEND_MESSAGE,
                     e.getMessage());
             } catch (InterruptedException e) {
@@ -39,6 +48,58 @@ public class FcmMessageSender implements SendMessagePort {
                 throw new FailedToSendMessageException(ErrorCode.ASYNC_ERROR, e.getMessage());
             }
         }, messageCallbackExecutor);
+    }
+
+    private void retrySendWithBackoff(Message message) {
+        int retryAttempt = 0;
+
+        while (retryAttempt < MAX_ATTEMPTS) {
+            int delay = calculateRetryDelay(retryAttempt);
+
+            sleep(delay);
+
+            if (resend(message)) {
+                break;
+            }
+            retryAttempt++;
+        }
+    }
+
+    private boolean resend(Message message) {
+        try {
+            String response = firebaseMessaging.send(message);
+            return response != null;
+        } catch (FirebaseMessagingException e) {
+            log.error("Failed to resend message: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    private boolean shouldRetry(Exception e) {
+        if (e.getCause() instanceof FirebaseMessagingException fme) {
+            MessagingErrorCode errorCode = fme.getMessagingErrorCode();
+            return isRetryErrorCode(errorCode);
+        }
+        return false;
+    }
+
+    private boolean isRetryErrorCode(MessagingErrorCode errorCode) {
+        return errorCode == MessagingErrorCode.INTERNAL || errorCode == MessagingErrorCode.UNAVAILABLE;
+    }
+
+    private int calculateRetryDelay(int retryAttempt) {
+        double exponentialDelay = INITIAL_DELAY_MS * Math.pow(DELAY_MULTIPLIER, retryAttempt);
+        double randomJitter = Math.random() * INITIAL_DELAY_MS;
+        return (int) (exponentialDelay + randomJitter);
+    }
+
+    private void sleep(int delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
     }
 
 }
