8149cdbe3f0e0aab8ff435b4dc95af8f787447d5
JIUNG YANG <yangjiung286@gmail.com>
2025-04-06 01:18:59 +0900

    BV2-223 (Feat) : 구독 실패 예외 처리

--- token/src/main/java/token/core/common/exception/TokenExceptionHandler.java
+++ token/src/main/java/token/core/common/exception/TokenExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import token.core.common.exception.topic.FailedToSubscribeTopicException;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,6 +20,14 @@ public class TokenExceptionHandler {
         log.error("", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(Api.ERROR(FcmTokenErrorCode.FIREBASE_INITIALIZATION_FAILED));
+    }
+
+    @ExceptionHandler(value = FailedToSubscribeTopicException.class)
+    public ResponseEntity<Api<Object>> failedToSubscribeToTopicException(
+        FailedToSubscribeTopicException e) {
+        log.error("", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(Api.ERROR(FcmTokenErrorCode.FAILED_TO_SUBSCRIBE_TOPIC));
     }
 
 }
