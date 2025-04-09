66516f90433fce97be2ead1582052fd3562bc488
JIUNG YANG <yangjiung286@gmail.com>
2025-04-09 20:59:50 +0900

    BV2-234 (Refactor) : TokenClient 미사용 메서드 제거

--- message/src/main/java/message/adapter/output/client/TokenClient.java
+++ message/src/main/java/message/adapter/output/client/TokenClient.java
@@ -1,8 +1,6 @@
 package message.adapter.output.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,9 +8,6 @@ import java.util.List;
 
 @FeignClient(name = "baobab-fcm-tokens")
 public interface TokenClient {
-
-    @GetMapping("/{userId}")
-    String getFcmToken(@PathVariable("userId") String userId);
 
     @PostMapping("/fcm-tokens")
     List<String> getFcmTokens(@RequestBody List<String> userIds);
