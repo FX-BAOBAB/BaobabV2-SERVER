d6a608add7aac4e48a218ded9f78a3eef205e3b9
SEOB <svj7531@naver.com>
2025-06-05 17:44:33 +0900

    BV2-259 (Feat) : 게시글 신고 요청에 대한 유효성 검사 추가
    BV2-259 (Feat) : 게시글 신고 요청에 대한 유효성 검사 추가

--- article/src/main/java/article/adapter/input/web/request/ArticleReportSaveRequest.java
+++ article/src/main/java/article/adapter/input/web/request/ArticleReportSaveRequest.java
@@ -1,6 +1,9 @@
 package article.adapter.input.web.request;
 
 import article.adapter.output.persistence.enums.ReportReasonType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +13,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ArticleReportSaveRequest {
 
+    @NotNull(message = "필수 입력 사항입니다.")
     private ReportReasonType reasonType;
 
+    @NotBlank(message = "필수 입력 사항입니다.")
+    @Size(max = 200, message = "최대 200자까지 입력 가능합니다.")
     private String reason;
 
+    @NotBlank(message = "필수 입력 사항입니다.")
     private String articleId;
 
 }
